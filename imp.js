ebc6924db7967e45292a6ff708dfd2f2b20cc197
oreoluwade <oreoluwade@gmail.com>
2017-12-04 07:43:31 +0100

    export constants, import and run same constants.

---
+++ imp.js
@@ -0,0 +1,5 @@
+import { userinfo, passOnlyArrays } from './exp';
+
+const callFunction = passOnlyArrays([5, 4, 6]);
+
+console.log("userinfo: ", userinfo, "=======\n", "callFunction: ", callFunction);
