07f3c031385b21357122c47be90d6f53e0e4e0fb
oreoluwade <oreoluwade@gmail.com>
2017-12-05 08:07:06 +0100

    fix the linting/syntax warnings/problems in existing files

--- imp.js
+++ imp.js
@@ -3,3 +3,5 @@ import { userinfo, passOnlyArrays } from './exp';
 const callFunction = passOnlyArrays([5, 4, 6]);
 
 console.log("userinfo: ", userinfo, "=======\n", "callFunction: ", callFunction);
+
+var great = peace;
