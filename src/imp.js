36e01606bdf7b6cdfe55029cd7a9297283a6bb1d
oreoluwade <oreoluwade@gmail.com>
2017-12-06 09:06:21 +0100

    remove hanging files in src directory

--- src/imp.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-import { userinfo, passOnlyArrays } from './exp';
-
-const callFunction = passOnlyArrays([5, 4, 6]);
-
-console.log('userinfo: ', userinfo, '=======\n', 'callFunction: ', callFunction);
-
